3efaf6e3791ae87a2bd1706be64ad18cf76cc905
hwangjinseong <imhjs2023@gmail.com>
2022-07-13 11:32:12 +0900

    반복문

---
+++ study/반복문.js
@@ -0,0 +1,37 @@
+// 반복문 : 반복하는 것
+
+// for문
+for(let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+// while문 
+let i = 0;
+while(i < 10) {
+    console.log(i);
+    i++;
+}
+
+// do.. while문
+let j = 0;
+do {
+    i++;
+} while(i < 10)
+
+// <break, cotinue>
+
+// break : 멈추고 빠져나옴
+while(true) {
+    let answer = confirm('계속 할까요?')
+    if(!answer) {
+        break;
+    }
+}
+
+// continue : 멈추고 다음 반복 진행
+for(let i = 0; i < 10; i++) {
+    if(i%2) {
+        continue;
+    }
+    console.log(i);
+}
