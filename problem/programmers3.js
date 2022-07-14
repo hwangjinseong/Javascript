93d95bbd03412e113b0632e0e805d587ca3abf3c
hwangjinseong <imhjs2023@gmail.com>
2022-07-14 22:15:24 +0900

    3: 소수 찾기

---
+++ problem/programmers3.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    let primeNum = true;
+    let count = 0;
+    
+    for (let i = 2; i <= n; i++) {
+      for (let j = 2; j * j <= i; j++) {
+        if (i % j === 0) {
+          primeNum = false;
+          break;
+        }
+      }
+      if (primeNum === true) {
+        count += 1;
+      }   
+      primeNum = true;
+    }
+    return(count);
+    }
